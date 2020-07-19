75a899faeced722ab71ec3dac84bc0c2366b2ae7
kam1tsur3 <kam1tsur3@gmail.com>
2020-07-19 11:17:41 +0900

    complete chap9

---
+++ chapter9/921/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func open() {
+	file, err := os.Create("textfile.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	io.WriteString(file, "New file content\n")
+}
+
+func read() {
+	file, err := os.Open("textfile.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	fmt.Println("Readfile:")
+	io.Copy(os.Stdout, file)
+}
+
+func append() {
+	file, err := os.OpenFile("textfile.txt", os.O_RDWR|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	io.WriteString(file, "Appended content\n")
+}
+
+func main() {
+	/*
+		open()
+		read()
+	*/
+	append()
+}
---
+++ chapter9/954/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+func main() {
+	fmt.Println(filepath.Clean("./path/filepath/../path.go"))
+
+	abspath, _ := filepath.Abs("path/filepath_unix.go")
+	fmt.Println(abspath)
+
+	relpath, _ := filepath.Rel("/usr/local/go/src",
+		"/usr/local/go/src/path/filepath/path.go")
+
+	fmt.Println(relpath)
+}
